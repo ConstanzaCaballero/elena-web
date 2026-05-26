8ddd95c31e7ebd9480562d1009d9648b961faab0
Constanza <constanzacmadrid1@gmail.com>
2026-05-26 19:41:57 +0200

    Add no-cache headers to prevent stale browser cache

--- app.py
+++ app.py
@@ -5,7 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    response = render_template('index.html')
+    from flask import make_response
+    resp = make_response(response)
+    resp.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
+    resp.headers['Pragma'] = 'no-cache'
+    return resp
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
